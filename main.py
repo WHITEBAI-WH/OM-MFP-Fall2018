200ca96a5e11578c2703c32eef6cc5799b5c9f2f
danielmcadams86 <daniel.mcadams@ttu.edu>
2018-12-08 16:46:22 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from predfailure import PredictFail as pf
+
+predict = pf()
+predict.build_MLE()
+predict.calc_rul()
+predict.create_state_probs()
+predict.output_summary_table()
+#predict.create_plot()
+
+
+
+
+
+
+
+
+
+    
+    
+
+
